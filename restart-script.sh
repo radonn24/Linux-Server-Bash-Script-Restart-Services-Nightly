aebce8088fdeda6294d59a772b8c50a1301231c9
Richard Donnell <richardadonnell@gmail.com>
2023-07-31 14:31:06 -0500

    added error logging with timestamps

--- restart-script.sh
+++ restart-script.sh
@@ -8,15 +8,22 @@ service4="mysqld.service"
 
 # Function to check the status of a service
 check_service_status() {
-    echo "Checking the status of $1"
+    echo "$(date) - Checking the status of $1"
     systemctl status "$1"
 }
 
 # Function to restart a service
 restart_service() {
-    echo "Restarting $1"
+    echo "$(date) - Restarting $1"
     systemctl restart "$1"
 }
+
+# Log file
+log_file="restart-services.log"
+
+# Redirect output to the log file with timestamps
+exec >> "$log_file"
+exec 2>&1
 
 # Check the status of services before restarting
 check_service_status "$service1"
