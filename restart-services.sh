e241a56e5499dcd380bdefe31447f10276b529b2
Richard Donnell <richardadonnell@gmail.com>
2023-08-04 19:26:17 -0500

    formatting

--- restart-services.sh
+++ restart-services.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Log file
-LOG_FILE="restart-services.log"
+log_file="restart-services.log"
 
 # Load the list of the names of the services you want to manage
 services=($(cat restart-services.conf))
@@ -40,7 +40,7 @@ restart_service() {
 }
 
 # Redirect output to the log file with timestamps
-exec >> "$LOG_FILE"
+exec >> "$log_file"
 exec 2>&1
 
 # Check the status of services before restarting
